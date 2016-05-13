32e5130d57017c6d6b872d9edd7219dbf69b818d
ponycoder <571533442@qq.com>
2016-05-13 22:01:29 +0800

    init

---
+++ PonyFrameworkOnSwift/UIViewEx.swift
@@ -0,0 +1,16 @@
+//
+//  UIViewEx.swift
+//  PonyFrameworkOnSwift
+//
+//  Created by 小黑 on 16/5/13.
+//  Copyright © 2016年 王云鹏. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public extension UIView{
+    static func color() {
+        print("123")
+    }
+}
